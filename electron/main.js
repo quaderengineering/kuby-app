772f310e747bdfc2e29e0d1c249f4555eef5da41
Sahid Fakiry <sahid.fakiry@quaderengineering.ch>
2025-10-03 11:03:50 +0200

    feat(electron): add serialport for usb/com connectivity support

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,9 @@
 const { app, BrowserWindow, globalShortcut, ipcMain } = require("electron");
 const path = require("path");
 const { spawn } = require("child_process");
-const express = require('express');
+const express = require("express");
+const { SerialPort } = require("serialport");
+const usbDetect = require("usb-detection");
 
 const appServer = express();
 
@@ -14,26 +16,27 @@ function createWindow() {
     height: 800,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: false
+      nodeIntegration: false,
     },
   });
 
   // Load Angular build
   // mainWindow.loadFile(path.join(process.resourcesPath, "frontend", "./browser/index.html"));
-  appServer.use(express.static(path.join(process.resourcesPath, "frontend", "./browser")));
+  appServer.use(
+    express.static(path.join(process.resourcesPath, "frontend", "./browser"))
+  );
   // Always fallback to Angular index.html
   appServer.use((req, res) => {
-    res.sendFile(path.join(process.resourcesPath, "frontend", "browser", "index.html"));
+    res.sendFile(
+      path.join(process.resourcesPath, "frontend", "browser", "index.html")
+    );
   });
-
 
   appServer.listen(4200, () => console.log("Angular served inside Electron"));
 
-  mainWindow.loadURL('http://localhost:4200');
-
   //// FIXME: FOR DEVELOPMENT ONLY
+  mainWindow.loadURL("http://localhost:4200");
   mainWindow.webContents.openDevTools();
-
 
   mainWindow.setMenuBarVisibility(false);
 
@@ -43,8 +46,7 @@ function createWindow() {
   });
 }
 
-
-app.on("ready", () => {
+app.on("ready", async () => {
   // Start .NET API
   apiPath = path.join(process.resourcesPath, "backend", "quader-backend.exe");
 
@@ -59,6 +61,10 @@ app.on("ready", () => {
   });
 
   createWindow();
+
+  usbDetect.startMonitoring();
+
+  await refreshPorts();
 });
 
 app.on("window-all-closed", () => {
@@ -72,7 +78,18 @@ app.on("activate", () => {
   if (mainWindow === null) createWindow();
 });
 
+usbDetect.on("add", async () => await refreshPorts());
+usbDetect.on("remove", async () => await refreshPorts());
+
 ipcMain.on("get-usb-data", (event) => {
   const fakeData = { status: "ok", value: 42 };
   event.sender.send("usb-data-response", fakeData);
 });
+
+async function refreshPorts() {
+  const ports = await SerialPort.list();
+  console.log(
+    "Available ports:",
+    ports.map((p) => p.path)
+  );
+}
